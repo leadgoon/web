3c11882c2705af43ace3062bf48a1903e15e740d
leadgoon <50123185+leadgoon@users.noreply.github.com>
2021-03-19 17:15:36 +0900

    Update App.js

--- amplifyapp/src/App.js
+++ amplifyapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. ^_^
+          Edit <code>src/App.js</code> and save to reload. ^_^!
         </p>
         <a
           className="App-link"
